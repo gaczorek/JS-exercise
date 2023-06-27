c17376f9bf07399c622d2cdf998abc1c71e9bb5c
gaczorek <kamila.gaczorek@poczta.onet.pl>
2023-06-27 18:23:09 +0200

    color change optimization

--- mouseMove/main.js
+++ mouseMove/main.js
@@ -1,11 +1,16 @@
 document.addEventListener("mousemove", movingMouse);
 
 function movingMouse(e) {
-  document.querySelector("h1").innerText = `
-    Client X/Y: ${e.clientX}, ${e.clientY},
-    Page X/Y: ${e.pageX}, ${e.pageY},
-    Screen X/Y: ${e.screenX}, ${e.screenY}`;
-    document.body.style.backgroundColor = `rgb(${e.clientX}, ${e.clientY}, 100)`;
+  const x = e.clientX + 1;
+  const y = e.clientY + 1;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  const red = x/width * 100 ;
+  const green = y/height * 100;
+  const blue = (red + green) / 2;
+
+  document.querySelector("h1").innerText = `${x}, ${y}`;
+
+  document.body.style.backgroundColor = `rgb( ${red}%,${green}%,${blue}%)`;
 }
-
-
