b2ade86e3eb5b65550dda83ed4cf2fcd8bd37e97
gaczorek <kamila.gaczorek@poczta.onet.pl>
2023-06-27 18:03:16 +0200

    add changeable background color

--- mouseMove/main.js
+++ mouseMove/main.js
@@ -5,4 +5,7 @@ function movingMouse(e) {
     Client X/Y: ${e.clientX}, ${e.clientY},
     Page X/Y: ${e.pageX}, ${e.pageY},
     Screen X/Y: ${e.screenX}, ${e.screenY}`;
+    document.body.style.backgroundColor = `rgb(${e.clientX}, ${e.clientY}, 100)`;
 }
+
+
